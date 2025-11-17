8e9010e4c1e355cc51bf3c199f53addbf68504d1
Camilo Puerta <cpuertac@unicartagena.edu.co>
2025-11-17 16:49:04 -0500

    Creacion VIsta Controlador

---
+++ src/main/java/com/jcaa/hexagonal/ControladorInicio.java
@@ -0,0 +1,19 @@
+package com.jcaa.hexagonal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+
+    @GetMapping("/")
+    public String inicio() {
+        log.info("Ejecutando el controlador Inicio MVC");
+        return "index";
+    }
+
+}
